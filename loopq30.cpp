8ea0842c2e3e24e5c40e69cf57b31ec64f642465
nitinkumar2558 <42238704+nitinkumar2558@users.noreply.github.com>
2018-08-31 07:55:14 +0000

    Add files via upload

---
+++ loopq30.cpp
@@ -0,0 +1,19 @@
+//write a c++ program to print multiplication table of any number
+#include <iostream>
+using namespace std;
+int main () {
+//decleration of variables
+int n,i;
+cout << "please number whom multiplication table you want"<<endl;
+//input variables
+cin>>n;
+//assigning the value
+i=1;
+   
+while (i<=10)
+{    cout<< n<<" * "<< i << "=" << n*i << endl;
+    i=i+1;
+ 
+}
+return 0 ;
+}
